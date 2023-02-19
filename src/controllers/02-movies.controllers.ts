29543975341c2ef10d9d9473fa05975c61ef2703
castromaciel <castromaciel00@gmail.com>
2023-02-19 16:33:16 -0300

    feat: get movie by id

--- src/controllers/02-movies.controllers.ts
+++ src/controllers/02-movies.controllers.ts
@@ -14,3 +14,25 @@ export const getMovies = (req: Request, res: Response) => {
     ]
   })
 }
+
+export const getMovie = (req: Request, res: Response) => {
+  const { id } = req.params
+  if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+    const headers = new HeaderData({ status: 'error', message: 'ObjectId is not valid' })
+
+    return res.status(404).send({
+      headers,
+      data: []
+    })
+  }
+
+  const headers = new HeaderData({ status: 'success', message: `Movie with id ${id} retrieved successfully` })
+  return res.status(200).send({
+    headers,
+    movie: {
+      id: 1,
+      title: 'Random Movie',
+      description: 'Lorem ipsum dolor sit amet, consectetur adip'
+    }
+  })
+}
--- src/routes/02-movies.routes.ts
+++ src/routes/02-movies.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { getMovies } from '../controllers/02-movies.controllers'
+import { getMovie, getMovies } from '../controllers/02-movies.controllers'
 
 const router = Router()
 
 router.get('/', getMovies)
+router.get('/:id', getMovie)
 
 export default router
