560bc58b7cba2b40e43239632c80214ce44f1d70
castromaciel <castromaciel00@gmail.com>
2023-04-02 13:08:31 -0300

    fix: update controllers

--- src/controllers/01-ecommerce.controllers.ts
+++ src/controllers/01-ecommerce.controllers.ts
@@ -2,9 +2,8 @@ import {
   Request, Response
 } from 'express'
 import { STATUS } from '../constants'
-import { isValidId } from '../helpers/validateId'
-import { HeaderData } from '../models/header'
 import { Product } from '../models/Products'
+import { HeaderData } from '../models/header'
 
 export const getProducts = async (req: Request, res: Response) => {
   const { limit, from = 0 } = req.query
@@ -18,40 +17,22 @@ export const getProducts = async (req: Request, res: Response) => {
 
     res.status(200).send({
       headers,
-      data: products
+      products
     })
   } catch (error) {
     const headers = new HeaderData({ status: STATUS.error, message: 'Failed to fetch products' })
 
     res.status(400).send({
-      headers,
-      data: []
+      headers
     })
   }
 }
 
 export const getProduct = async (req: Request, res: Response) => {
   const { id } = req.params
-  if (!isValidId(id)) {
-    const headers = new HeaderData({ status: STATUS.error, message: 'Invalid id' })
-
-    return res.status(404).send({
-      headers,
-      data: []
-    })
-  }
 
   const product = await Product.findById({ _id: id })
-
-  if (!product) {
-    const headers = new HeaderData({ status: STATUS.error, message: `Product with id ${id} doesn't exist` })
-    return res.status(404).send({
-      headers,
-      data: []
-    })
-  }
-
-  const headers = new HeaderData({ status: STATUS.success, message: `Product with id ${id} retrieved successfully` })
+  const headers = new HeaderData({ status: STATUS.success, message: 'Product retrieved successfully' })
 
   return res.status(200).send({
     headers,
